3b0a20e4e4a1d92e1c6a60c980f2861b660cd19e
Dorian-Perthuis <dorian.perthuis@outlook.fr>
2023-05-14 19:38:22 -0400

    create hiAnimation component

---
+++ src/lib/hiAnimation.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    
+</script>
+
+<div class="hand">🖐</div>
+
+<style>
+    .hand{
+        font-size: 3rem;
+        transform-origin: 0 5rem;
+        animation: anim 1s ease-out;
+    }
+    @keyframes anim{
+        0%  { scale:0; rotate: 90deg; }
+        50% { scale:1 }
+        65% { rotate: -10deg }
+        80% { rotate:5deg }
+        95% { rotate:-2deg }
+    }
+</style>
