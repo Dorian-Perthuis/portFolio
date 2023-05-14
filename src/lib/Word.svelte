b9ce041a09b6e1f1aa43c930899e9eef95bb7ebf
Dorian-Perthuis <dorian.perthuis@outlook.fr>
2023-05-14 17:32:10 -0400

    Create word component

---
+++ src/lib/Word.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { fly } from 'svelte/transition';
+    export let visible = true;
+
+    function handleClick(event){
+        visible = !visible;
+    }
+</script>
+
+<div on:click={handleClick}>
+    {#if visible}
+    <div transition:fly="{{y:10, duration:300}}"><slot></slot></div>
+    {/if}
+</div>
+
+<style>
+    div{
+        position: relative;
+        width: fit-content;
+        height: fit-content;
+        overflow: hidden;
+    }
+
+    p{
+        position: absolute;
+    }
+</style>
