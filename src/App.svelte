03c0c0a66dc61d09cf2d7e877867273cecda0df5
Dorian-Perthuis <dorian.perthuis@outlook.fr>
2023-05-14 17:29:57 -0400

    create hero banner component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  
+  import HeroBanner from "./lib/heroBanner.svelte";
 </script>
 
 <main>
-  
+  <HeroBanner></HeroBanner>
 </main>
 
 <style>
---
+++ src/lib/heroBanner.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import profilPicture from "../assets/profilPicture.png"
+</script>
+
+<div class=container>
+    <img id="profilPicture" alt="dorian perthuis" src={profilPicture}>
+    <div id="description">
+        <p>test</p>
+    </div>
+</div>
+
+
+<style>
+    .container{
+        display: grid;
+        grid-template-areas: "profilPicture description end";
+        grid-template-columns: 20% auto 10%;
+        padding: 2rem;
+        gap: 2rem; 
+        height: 100vh;
+        width: 100vw;
+        box-sizing: border-box;
+    }
+
+    #profilPicture{
+        width: 100%;
+        aspect-ratio: 1;
+        align-self: center;
+    }
+
+</style>
