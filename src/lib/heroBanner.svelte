cfa121b712c8d5f0ee4c132b7c3657103d222a62
Dorian-Perthuis <dorian.perthuis@outlook.fr>
2023-05-14 17:39:13 -0400

    add text to display on hero banner

--- src/lib/heroBanner.svelte
+++ src/lib/heroBanner.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-    import profilPicture from "../assets/profilPicture.png"
+    import profilPicture from "../assets/profilPicture.png";
+    import Phrase from "./Phrase.svelte";
 </script>
 
 <div class=container>
     <img id="profilPicture" alt="dorian perthuis" src={profilPicture}>
     <div id="description">
-        <p>test</p>
+        <Phrase>Hi !</Phrase>
+        <Phrase>My name is Dorian, a French Guy,</Phrase>
+        <Phrase>a future software engineer.</Phrase>
+        <Phrase>I am creative developer lover.</Phrase>
+        <Phrase>Let me show you my works...</Phrase>
     </div>
 </div>
 
@@ -26,6 +31,11 @@
         width: 100%;
         aspect-ratio: 1;
         align-self: center;
+    }
+
+    #description{
+        display: flex;
+        flex-direction: column;
     }
 
 </style>
