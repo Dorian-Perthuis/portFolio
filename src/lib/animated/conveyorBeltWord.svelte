95be48fb401b3f59008e988aab9f70a55042a2c6
Dorian-Perthuis <dorian.perthuis@outlook.fr>
2023-05-23 20:47:27 -0400

    fix converorBeltWord to adapt to any string

--- src/lib/animated/conveyorBeltWord.svelte
+++ src/lib/animated/conveyorBeltWord.svelte
@@ -6,25 +6,33 @@
     export let space:number = 5;
     export let nb:number = 1;
     export let time:number = 3;
-    export let lenght:number = 50;
+    let lenght:number;
 
     let container:HTMLElement;
-
+    let wordWidth:number;
+    let sepWidth:number;
     
     onMount(()=>{
+        lenght = wordWidth+sepWidth+2*space;
         container.style.cssText += `
             --space:${space}px;
             --time:${time}s;
             --lenght:${lenght}px;
         `;
     });
+
+    function hoverHandle(e){
+        console.log(wordWidth);
+        console.log(sepWidth);
+    }
     
 </script>
 
-    <div class="container" bind:this={container}>
+    <div on:mouseenter={hoverHandle} class="container" bind:this={container}>
         <div class="text">
-            {#each Array(nb) as _, index (index)}
-               <span>{word}</span><pan>{separator} </pan>
+            <span bind:offsetWidth={wordWidth}>{word}</span><span bind:offsetWidth={sepWidth}>{separator} </span>
+            {#each Array(nb-1) as _, index (index)}
+               <span id={`w-${index}`}>{word}</span><span id={`s-${index}`}>{separator} </span>
             {/each}
         </div>
     </div>
